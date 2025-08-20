4931a0ae34a18466eb95de9552fc2ce9cc403749
woodward42 <woodward42@users.noreply.github.com>
2025-08-20 11:32:50 +0300

    [3-calc-events][done]

---
+++ 3-calc-events/index.js
@@ -0,0 +1,39 @@
+import { EventEmitter } from 'events';
+const emitter = new EventEmitter();
+
+emitter.on("result", (res) => {
+  console.log("ИТОГ", res);
+});
+
+emitter.on("add", (a, b) => {
+  emitter.emit("result", firstNum + secondNum);
+});
+
+emitter.on("multiply", (a, b) => {
+  emitter.emit("result", firstNum * secondNum);
+});
+
+//переданные аргументы
+const args = process.argv.slice(2);
+
+//первое число
+const firstNum = +args[0];
+
+//второе число
+const secondNum = +args[1];
+
+//операция
+const operation = args[2];
+
+switch (operation) {
+  case "add":
+    emitter.emit("add", firstNum, secondNum);
+    break;
+
+  case "multiply":
+    emitter.emit("multiply", firstNum, secondNum);
+    break;
+
+  default:
+    console.log("no such operation");
+}
